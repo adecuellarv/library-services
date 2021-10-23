785ad85840fa80b60eebdbc4c515f809f904f582
adecuellarv <76180286+adecuellarv@users.noreply.github.com>
2021-10-23 03:05:00 -0500

    configuración de endpoints

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,48 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('api')
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @Get('/books')
+  getBooks() {
+    return [
+      {
+        id: 1,
+        title: 'Fly 1'
+      }
+    ]
+  }
+
+  @Get('/categories')
+  getCategories() {
+    return [
+      {
+        id: 1,
+        categoryName: 'Infantil'
+      },
+      {
+        id: 2,
+        categoryName: 'Ciencia ficción'
+      }
+    ]
+  }
+
+  @Post('/new-user')
+  addUser(@Body() body: any){
+    return body;
+  }
+
+  @Post('/login')
+  loginUser(@Body() body: any){
+    return body;
+  }
+
+  @Put('/update-user:id')
+  updateUser(@Param('id') id: number, @Body() body: any){
+    return body;
+  }
 
   @Get()
   getHello(): string {
